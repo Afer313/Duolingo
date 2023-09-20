8cb72f848fc20dfccda3c5f5fcd562d552530608
Afer <118452356+Afer313@users.noreply.github.com>
2023-09-21 01:13:27 +0400

    Update script.js v.0.01

--- script.js
+++ script.js
@@ -1,77 +1,82 @@
-let answersContainer = document.getElementById("answersContainer");
-let output1 = document.getElementById("output1");
-let output2 = document.getElementById("output2");
+const answersContainer = document.getElementById("answersContainer");
+const output1 = document.getElementById("output1");
+const output2 = document.getElementById("output2");
+const heartsCountElement = document.getElementById("heartsCountElement");
+const successContainer = document.getElementById("successContainer");
+const failedContainer = document.getElementById("failedContainer");
+const levelScoreIndicator = document.getElementById('levelScoreIndicator');
+let indicator = 0;
+
+const answersArr = ["Onun", "edersiz", "Boz", "eder", "ederler", "işi", "Baz", "devam"];
+const rightAnswer = ["Onun", "işi", "devam", "eder"];
+
 let heartCount = 5;
-let heartsCountElement = document.getElementById("heartsCountElement");
-
-const answersArr = [
-  "Onun",
-  "edersiz",
-  "Boz",
-  "eder",
-  "ederler",
-  "işi",
-  "Baz",
-  "devam",
-];
-
-const answersLoad = () => {
-  answersContainer.innerHTML = `<div></div>`;
-  answersArr.forEach(
-    (answer) =>
-      (answersContainer.innerHTML += `<div onclick='detonate(${answersArr.indexOf(
-        answer
-      )})' class='answer-cloud'>${answer}</div>`)
-  );
-};
-
 let forOutput = [];
 let forOutput2 = [];
-let flag = -1;
-let blackFlag = -1;
-let forOutputCount = 0;
-let forOutput2Count = 0;
+
+function moveWordToFirstLine() {
+  if (forOutput.length < 4 && forOutput2.length > 0) {
+    const wordToMove = forOutput2.shift(); 
+    forOutput.unshift(wordToMove); 
+    updateOutputFields(); 
+  }
+}
+
+function answersLoad() {
+  answersContainer.innerHTML = "";
+  answersArr.forEach((answer, index) => {
+    if (!forOutput.includes(answer) && !forOutput2.includes(answer)) {
+      const answerElement = document.createElement("div");
+      answerElement.className = "answer-cloud";
+      answerElement.textContent = answer;
+      answerElement.onclick = () => detonate(index);
+      answersContainer.appendChild(answerElement);
+    }
+  });
+}
 
 function detonate(index) {
-  if (forOutputCount <= 3) {
-    flag++;
-    const selectedWord = answersArr[index];
+  const selectedWord = answersArr[index];
+
+  if (forOutput.length < 4) {
     forOutput.push(selectedWord);
-    forOutputCount++;
-    output1.innerHTML += `<div onclick='closeMe(this, "${selectedWord}")' id='a${flag}' class='answer-cloud'>${selectedWord}</div>`;
-    answersArr.splice(index, 1);
-  } else if (forOutput2Count <= 3) {
-    blackFlag++;
-    const selectedWord = answersArr[index];
+  } else if (forOutput2.length < 4) {
     forOutput2.push(selectedWord);
-    forOutput2Count++;
-    output2.innerHTML += `<div onclick='closeMe2(this, "${selectedWord}")' id='b${blackFlag}' class='answer-cloud'>${selectedWord}</div>`;
-    answersArr.splice(index, 1);
   } else {
     alert("OK?");
   }
-  answersLoad();
+  moveWordToFirstLine();
+
+  const selectedElement = answersContainer.children[index];
+  if (selectedElement) {
+    selectedElement.classList.add("grayed-box");
+  }
+  updateOutputFields();
 }
 
-function closeMe(element, word) {
-  answersArr.push(word);
-  output1.removeChild(element);
-  answersLoad();
-  forOutputCount--;
-  forOutput.pop();
+function updateOutputFields() {
+  output1.innerHTML = forOutput.map(word => `<div onclick="giveMeBackPlease('${word}', 1)" class="answer-cloud">${word}</div>`).join("");
+  output2.innerHTML = forOutput2.map(word => `<div onclick="giveMeBackPlease('${word}', 2)" class="answer-cloud">${word}</div>`).join("");
 }
 
-function closeMe2(element, word) {
-  answersArr.push(word);
-  output2.removeChild(element);
-  answersLoad();
-  forOutput2Count--;
-  forOutput2.pop();
-}
+function giveMeBackPlease(wordValue, set) {
+  const selectedArray = set === 2 ? forOutput2 : forOutput;
+  const elementsInsideContainer = answersContainer.getElementsByTagName("*");
+  for (let i = 0; i < elementsInsideContainer.length; i++) {
+    const element = elementsInsideContainer[i];
+    if (element.innerHTML === wordValue) {
+      element.classList.remove('grayed-box');
+    }
+  }
 
-answersLoad();
-const rightAnswer = ["Onun", "işi", "devam", "eder"];
-let finalAnswer = [];
+  const indexToRemove = selectedArray.indexOf(wordValue);
+  if (indexToRemove !== -1) {
+    selectedArray.splice(indexToRemove, 1);
+  }
+
+  updateOutputFields();
+  moveWordToFirstLine();
+}
 
 function control() {
   finalAnswer = forOutput.concat(forOutput2);
@@ -93,6 +98,10 @@ function control() {
     }
     if (correct) {
       success();
+      if(indicator<100){
+        indicator +=10;
+      }
+      levelScoreIndicator.style.width = `${indicator}%`;
     } else {
       failed();
       heartCount--;
@@ -106,21 +115,22 @@ function control() {
 }
 
 function success() {
-  document.getElementById("successContainer").style.bottom = "0px";
+  successContainer.style.bottom = "0px";
 }
 function failed() {
-  document.getElementById("failedContainer").style.bottom = "0px";
+  failedContainer.style.bottom = "0px";
 }
 function tryAgain() {
-  document.getElementById("failedContainer").style.bottom = "-110px";
+  failedContainer.style.bottom = "-110px";
 }
 function newLvl() {
-  document.getElementById("successContainer").style.bottom = "-110px";
+  successContainer.style.bottom = "-110px";
 }
 
 function gameOver() {
   if (heartCount == 0) {
-    document.body.innerHTML =
-      "<div><h1 style='margin: 100px auto; font-size: 140px; color:red;'>GAME OVER!</h1></div>";
+    document.body.innerHTML = "<div><h1 style='margin: 100px auto; font-size: 140px; color:red;'>GAME OVER!</h1></div>";
   }
 }
+
+answersLoad();
